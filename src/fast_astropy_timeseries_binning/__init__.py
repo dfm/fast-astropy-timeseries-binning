ad4f652fb46ef1850e306e590839e017b3bcdaba
Dan F-M <foreman.mackey@gmail.com>
2023-05-02 11:02:34 -0400

    initial

---
+++ src/fast_astropy_timeseries_binning/__init__.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# from fast_astropy_timeseries_binning._fast_astropy_timeseries_binning import reduceat
+from fast_astropy_timeseries_binning.version import __version__ as __version__
+
+
+# class reducer:
+#     @staticmethod
+#     def reduceat(array, indices):
+#         out = np.empty(indices.shape, dtype=array.dtype)
+#         reduceat(array, indices, out)
+#         return out
