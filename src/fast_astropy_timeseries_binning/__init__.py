58d13bda20b36ac2047bc89e8aa6bedd5daeac0a
Dan F-M <foreman.mackey@gmail.com>
2023-05-02 11:39:21 -0400

    adding reducer

--- src/fast_astropy_timeseries_binning/__init__.py
+++ src/fast_astropy_timeseries_binning/__init__.py
@@ -1,12 +1,2 @@
-import numpy as np
-
-# from fast_astropy_timeseries_binning._fast_astropy_timeseries_binning import reduceat
 from fast_astropy_timeseries_binning.version import __version__ as __version__
-
-
-# class reducer:
-#     @staticmethod
-#     def reduceat(array, indices):
-#         out = np.empty(indices.shape, dtype=array.dtype)
-#         reduceat(array, indices, out)
-#         return out
+from fast_astropy_timeseries_binning.reducer import reducer as reducer
